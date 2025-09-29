7385455879821fbe55cc74472edd3044a04b2b52
Gutemberg Rocha <gutemberg149@gmail.com>
2025-09-29 18:40:47 -0300

    menu e bacalho responsividade feito

--- access-control-cp/src/components/Cabecalho/Cabecalho.tsx
+++ access-control-cp/src/components/Cabecalho/Cabecalho.tsx
@@ -3,8 +3,8 @@ import Menu from "../Menu/Menu";
 
 export default function Cabecalho() {
     return (
-        <header className="w-full bg-blue-200 flex flex-col justify-around items-center h-[80px] ">
-            <h1 className="text-2xl font-bold text-blue-800">Control Acess</h1>
+        <header className="w-full bg-blue-200 flex flex-col justify-around items-center h-[90px] ">
+            <h1 className="text:xl md:text-2xl font-bold text-blue-800">Control Acess</h1>
             <Menu/>
         </header>
     );
--- access-control-cp/src/components/Menu/Menu.tsx
+++ access-control-cp/src/components/Menu/Menu.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Menu() {
     return(
-        <nav className="menu-nav w-[40%] flex flex-row justify-around mb-1.5">
-            <Link to="/login" className="font-bold px-3 py-2 bg-white rounded-xl hover:bg-blue-300 "> Login </Link>
-            <Link to="/cadastro" className="font-bold px-3 py-2 bg-white rounded-xl hover:bg-blue-300 "> Cadastro </Link>
+        <nav className="menu-nav w-[375px] md:w-[40%] lg:w-[30%]  flex flex-row justify-around mb-1.5">
+            <Link to="/login" className="font-bold text-sm md:text-xl px-2 py-1 md:px-3  md:py-2 bg-white rounded-xl 
+              hover:bg-blue-300"> Login </Link>
+            <Link to="/cadastro" className="font-bold text-sm md:text-xl px-2 py-1 md:px-3  md:py-2 bg-white rounded-xl 
+              hover:bg-blue-300"> Cadastro </Link>
         </nav>
     )
 }
