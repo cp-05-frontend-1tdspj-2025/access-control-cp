c222d07e23e71e064cf9f69275e5fc57364552e0
JoaoHBatistaLeal <jhbatistaleal@gmail.com>
2025-10-03 09:17:24 -0300

    style(rodape): estiliza o componente rodape

--- access-control-cp/src/components/Rodape/Rodape.tsx
+++ access-control-cp/src/components/Rodape/Rodape.tsx
@@ -1,7 +1,7 @@
 export default function Rodape() {
     return (
-        <footer>
-            <p>&copy; 2025 1TDSPJ Access Control. Todos os direitos reservados.</p>
+        <footer className="w-full bg-cyan-400 flex flex-col justify-around items-center h-[90px] ">
+            <p className="text:xl md:text font-bold text-blue-800">&copy; 2025 1TDSPJ Access Control. Todos os direitos reservados.</p>
         </footer>
     );
 }
