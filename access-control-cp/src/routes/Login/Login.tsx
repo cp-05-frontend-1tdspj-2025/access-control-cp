a4cf9c9f0b24be34709f7c6d6041f381c2f45f93
JoaoHBatistaLeal <jhbatistaleal@gmail.com>
2025-10-03 08:08:25 -0300

    style(login) estiliza campos de inputs da rota login com inline tailwindcss

--- access-control-cp/src/routes/Login/Login.tsx
+++ access-control-cp/src/routes/Login/Login.tsx
@@ -12,34 +12,40 @@ export default function Login(){
   
 };
 
-
     return(
-     <main>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+     <main className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
 
         <div>
-          <label htmlFor="nomeUsuario">Nome de usuário:</label>
-          <input
-            id="nomeUsuario"
-            {...register('nomeUsuario', { required: 'Nome de usuário é obrigatório' })}
-          />
-          {errors.nomeUsuario && (
-            <span style={{ color: 'red' }}>{errors.nomeUsuario.message}</span>
+          <label htmlFor="nomeUsuario" className="block text-sm font-medium text-gray-700">
+              Nome de usuário:
+            </label>
+            <input
+              id="nomeUsuario"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              {...register('nomeUsuario', { required: 'Nome de usuário é obrigatório' })}
+            />
+            {errors.nomeUsuario && (
+              <span className="text-red-500 text-sm mt-1 block">{errors.nomeUsuario.message}</span>
           )}
         </div>
 
 
         <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            {...register('email', {
-              required: 'Email é obrigatório',
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: 'Email inválido'
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email:
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              {...register('email', {
+                required: 'Email é obrigatório',
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: 'Email inválido'
               }
             })}
           />
@@ -49,8 +55,10 @@ export default function Login(){
         </div>
 
   
-        <button type="submit">Entrar</button>
+        <button type="submit" className="w-full bg-cyan-400 text-white py-2 px-4 rounded-md hover:bg-blue-800 transition-colors">Entrar</button>
+
       </form>
+      </div>
     </main>
   );
 }
