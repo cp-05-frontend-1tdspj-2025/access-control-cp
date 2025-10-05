aafea2e7917eb83cbe85f951bcd4853cc6d9c830
hector <hectorsoria2@outlook.com>
2025-10-05 20:31:40 -0300

    Criacao do botao Home, e inicio da pagna Home

--- access-control-cp/src/components/Menu/Menu.tsx
+++ access-control-cp/src/components/Menu/Menu.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function Menu() {
     return(
         <nav className="menu-nav w-[375px] md:w-[40%] lg:w-[30%]  flex flex-row justify-around mb-1.5">
+            <Link to="/" className="font-bold text-sm md:text-xl px-2 py-1 md:px-3  md:py-2 bg-white rounded-xl hover:bg-blue-300"> Home </Link>
             <Link to="/login" className="font-bold text-sm md:text-xl px-2 py-1 md:px-3  md:py-2 bg-white rounded-xl hover:bg-blue-300"> Login </Link>
             <Link to="/cadastro" className="font-bold text-sm md:text-xl px-2 py-1 md:px-3  md:py-2 bg-white rounded-xl hover:bg-blue-300"> Cadastro </Link>
             <Link to="/integrantes" className="font-bold text-sm md:text-xl px-2 py-1 md:px-3  md:py-2 bg-white rounded-xl hover:bg-blue-300"> Integrantes </Link>
