a5a6a74e79186b999f3572e16f03d6d6bd218c24
erik <erikmiyasato@gmail.com>
2025-10-05 21:37:03 -0300

    Adicionando conteudo a pagina home do projeto

--- access-control-cp/src/routes/Home/Home.tsx
+++ access-control-cp/src/routes/Home/Home.tsx
@@ -10,6 +10,19 @@ export default function Home(){
         <div>
           <p>Bem-vindo ao sistema de controle de acesso!</p>
           <p>Use o menu para navegar entre as páginas.</p>
+          <p>Esse site foi desenvolvido como parte de uma prova para Frontend.</p>
+          <p>Use o menu a cima para acessar as funcionalidades do sistema.</p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h2>Recursos do Sistema</h2>
+          <ul>
+            <li>Cadastro de usuários</li>
+            <li>Login de usuários</li>
+            <li>Listagem de integrantes do grupo</li>
+          </ul>
         </div>
       </section>
     </main>
